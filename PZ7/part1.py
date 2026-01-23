e59e06b73cd0bb73a81524a5d194aed69e0b116c
deslaksy <shopaqwe294@gmail.com>
2026-01-23 17:51:05 +0300

    Update part1.py

--- PZ7/part1.py
+++ PZ7/part1.py
@@ -1,6 +1,11 @@
+try:
 s = input("Введите строку:")
+if not s:
+else:
 count = 0
 for c in s:
     if 'A' <= c <= 'Z':
         count += 1
         print("Количество заглавных латинских букв",count)
+except exception as e:
+print("Error")
