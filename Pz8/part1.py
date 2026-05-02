c2da538c2b6cb450d692fc515ef5a2fe0ee4f95c
deslaksy <shopaqwe294@gmail.com>
2026-05-02 15:42:39 +0300

    Update and rename part1 to part1.py

---
+++ Pz8/part1.py
@@ -0,0 +1,11 @@
+# Генерация словаря
+generated_dict = {}
+for i in range(7):
+    generated_dict[i] = i ** 3
+
+# Отображение исходного словаря
+print("Исходный словарь:", generated_dict)
+del generated_dict
+del generated-dict
+print("Получившийся словарь:", generated_dict)
+
