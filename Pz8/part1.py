5f1b2ce4edce78c8468431d0e2686640284772c9
deslaksy <shopaqwe294@gmail.com>
2026-05-02 15:49:23 +0300

    Update part1.py

--- Pz8/part1.py
+++ Pz8/part1.py
@@ -1,11 +1,4 @@
-# Генерация словаря
-generated_dict = {}
-for i in range(7):
-    generated_dict[i] = i ** 3
-
-# Отображение исходного словаря
-print("Исходный словарь:", generated_dict)
-del generated_dict
-del generated-dict
-print("Получившийся словарь:", generated_dict)
-
+# Создаём словарь
+d = {'a': 1, 'b': 2}
+keys_list = list(d.keys())
+print(keys_list)
