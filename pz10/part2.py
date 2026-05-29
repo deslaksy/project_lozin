c32554b2f1f17372df887bb6206d322081cb6dd4
deslaksy <shopaqwe294@gmail.com>
2026-05-29 17:31:20 +0300

    Update part2.py

--- pz10/part2.py
+++ pz10/part2.py
@@ -1,6 +1,6 @@
 spaces = 0
 
-# вывод и подсчет пробелов
+# подсчет пробелов
 for line in open('text18-14.txt', encoding='UTF-8'):
     print(line, end='')
     for ch in line:
@@ -14,14 +14,12 @@ f1 = open('text18-14.txt', encoding='UTF-8')
 l = f1.readlines()
 f1.close()
 
-# заменяем 3 строку на коды
+# заменяем 3 строку на код
 if len(l) >= 3:
     new_line = ''
     for ch in l[2]:
         new_line += str(ord(ch)) + ' '
     l[2] = new_line + '\n'
-
-# запись
 f2 = open('result.txt', 'w')
 f2.writelines(l)
 f2.close()
