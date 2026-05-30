3b441d7a58ac5b5a48f6c7d02f8c249a2a3d4f1e
deslaksy <shopaqwe294@gmail.com>
2026-05-30 15:48:28 +0300

    Update part1.py

--- pz10/part1.py
+++ pz10/part1.py
@@ -1,22 +1,15 @@
-#Создаём исходный файл с числами
+#Средствами языка Python сформировать текстовый файл (.txt), содержащий  последовательность из целых положительных и отрицательных чисел. Сформировать новый текстовый файл (.txt) следующего вида, предварительно выполнив требуемую  обработку элементов:
 numbers = [3, -5, 7, -2, 9, -8, 4]  
 with open('input.txt', 'w') as file:
     for num in numbers:
         file.write(f"{num} ")
-# Читаем файл, обрабатываем данные
 with open('input.txt', 'r') as file:
     data = file.read().split()
     numbers = [int(x) for x in data]
-
-# Вычисляем требуемые значения
 count = len(numbers) 
 min_value = min(numbers)  
 min_index = numbers.index(min_value) 
-
-# Умножаем все элементы на минимальный элемент
 processed_numbers = [num * min_value for num in numbers]
-
-# Формируем новый файл с результатами
 with open('output.txt', 'w') as file:
     file.write("Исходные данные:\n")
     file.write(f"Количество элементов: {count}\n")
